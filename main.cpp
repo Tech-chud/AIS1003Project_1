481ee9c5a5c56ff0521b0193d82592c01de7ecf4
TechChud <borgarkl@outlook.com>
2024-10-05 16:15:26 +0200

    FirstCommit
    -empty

--- main.cpp
+++ main.cpp
@@ -1,8 +1,65 @@
-#include <iostream>
+#include "threepp/threepp.hpp"
 
-//test
+using namespace threepp;
+
+auto createBox(const Vector3& pos, const Color& color) {
+    auto geometry = WireframeGeometry::create();
+    auto material = MeshPhongMaterial::create();
+    material->color = color;
+
+    auto box = Mesh::create(geometry, material);
+    box->position.copy(pos);
+
+    return box;
+}
+
+auto createPlane() {
+    auto planeGeometry = PlaneGeometry::create(5, 5);
+    auto planeMaterial = MeshLambertMaterial::create();
+    planeMaterial->color = Color::gray;
+    planeMaterial->side = Side::Double;
+
+    auto plane = Mesh::create(planeGeometry, planeMaterial);
+    plane->position.y = -1;
+    plane->rotateX(math::degToRad(90));
+
+    return plane;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    Canvas canvas("Demo");
+    GLRenderer renderer{canvas.size()};
+
+    auto scene = Scene::create();
+    auto camera = PerspectiveCamera::create(75, canvas.aspect(), 0.1f, 100);
+    camera->position.z = 5;
+
+    OrbitControls controls{*camera, canvas};
+
+    auto light = HemisphereLight::create();
+    scene->add(light);
+
+    auto plane = createPlane();
+    scene->add(plane);
+
+    auto group = Group::create();
+    group->add(createBox({-1, 0, 0}, Color::green));
+    group->add(createBox({1, 0, 0}, Color::red));
+    scene->add(group);
+
+    canvas.onWindowResize([&](WindowSize size) {
+        camera->aspect = size.aspect();
+        camera->updateProjectionMatrix();
+        renderer.setSize(size);
+    });
+
+    Clock clock;
+    canvas.animate([&]() {
+
+        float dt = clock.getDelta();
+        group->rotation.y += 1.f * dt;
+
+        renderer.render(*scene, *camera);
+    });
 }
