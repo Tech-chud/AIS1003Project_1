2b6c1eb9d46fbafef86ec1cc0974884be87b6477
borga <borgarkl@outlook.com>
2024-11-27 12:40:27 +0100

    Newly added:
    -Fixed coliding problem
    
    To be implemented next:
    
    -Hp and score
    
    -Add tests
    
    -Updated models for all objects
    
    Lesser goals:
    
    -Asteroids splitting into two smaller ones when shot by player.
    
    -Menu for general testing with sliders for:
    Spawn rate, Velocity, Damage and general useful settings

--- Headers/GameSystem/HUD.h
+++ Headers/GameSystem/HUD.h
@@ -1,7 +1,31 @@
+#ifndef HUD_HPP
+#define HUD_HPP
 
-#ifndef HUD_H
-#define HUD_H
+#include "threepp/threepp.hpp"
+#include <memory>
+#include <string>
 
+class HUD {
+public:
+    HUD(const std::shared_ptr<threepp::Canvas>& canvas);
 
+    void updateScore(int score);
+    void updateHealth(int health);
+    void updateTime(float timeAlive);
 
-#endif //HUD_H
+    void render();
+
+private:
+    std::shared_ptr<threepp::Canvas> canvas_;
+    std::shared_ptr<threepp::Text> scoreText_;
+    std::shared_ptr<threepp::Text> healthText_;
+    std::shared_ptr<threepp::Text> timeText_;
+
+    int score_;
+    int health_;
+    float timeAlive_;
+
+    void updateTextElements();
+};
+
+#endif // HUD_HPP
